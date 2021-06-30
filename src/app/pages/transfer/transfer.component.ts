03e0f7d19ff56cb50044f49551ad6ebefdaad7be
rarahim <rarahim75@gmail.com>
2021-06-30 10:13:40 +0800

    Add codes to implement fund transfer to account number.

--- src/app/pages/transfer/transfer.component.ts
+++ src/app/pages/transfer/transfer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Services } from '../../services/service';
+import { NgPopupsService } from 'ng-popups';
 
 @Component({
   selector: 'app-transfer',
@@ -10,7 +12,7 @@ export class TransferComponent implements OnInit {
   // member: Member;
   public transferForm: any = {};
 
-  constructor(private service: Services) { }
+  constructor(private service: Services, private router: Router, private ngPopups: NgPopupsService) { }
   ngOnInit(): void {
     this.service.forms.transferForm = this.transferForm;
     this.service.opsTagging = 'transfer';
@@ -27,12 +29,23 @@ export class TransferComponent implements OnInit {
     const month = today.getMonth() + 1;
     this.transferForm.effectiveDate = (day < 10 ? "0" : "") + day + "/" + (month < 10 ? "0" : "") + month + "/" + today.getFullYear();
   }
-  memberChange(): void {
-    const selected = this.membersList.find(member => member.id === this.transferForm.toMemberId);
-    if (selected) {
-      this.transferForm.toMemberPrincipal = selected.name;
-      this.transferForm.selectedMember = selected;
-    }
-  }
 
+  // Commented out temporarily. (rwa)
+  // memberChange(): void {
+  //   const selected = this.membersList.find(member => member.id === this.transferForm.toMemberId);
+  //   if (selected) {
+  //     this.transferForm.toMemberPrincipal = selected.name;
+  //     this.transferForm.selectedMember = selected;
+  //   }
+  // }
+
+  async getReceiverDetails(): Promise<void> {
+    await this.service.getMemberByAccountNumber(this.transferForm.toAccountNo).toPromise()
+    .then(() => {
+      this.router.navigate(['transfer-details']);
+    })
+    .catch((err) => {
+      this.ngPopups.alert('There was an error in your submission!');
+    });
+  }
 }
