300bc010bc48dc3300889ff8e8f858b31b4b8edf
Aiman Wafi <aimanwafi@komeps.com>
2021-11-09 15:12:34 +0800

    add merchantName and amount for topup-info page

--- src/app/pages/topup-info/topup-info.component.ts
+++ src/app/pages/topup-info/topup-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Services } from 'src/app/services/service';
 
 @Component({
   selector: 'app-topup-info',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topup-info.component.scss']
 })
 export class TopupInfoComponent implements OnInit {
-
-  constructor() { }
+  amount:any
+  receiverName:any
+  merchantName:any
+  memberId
+  constructor(private services:Services, private router: Router) { }
 
   ngOnInit(): void {
+    ;
+    this.amount = this.router.url.split("?")[1].split("=")[2];
+    this.services.loadById(this.router.url.split("?")[1].split("=")[1].split("&")[0]).subscribe((res: any) => {
+      console.log(res);
+      this.memberId= res.id;
+      this.receiverName= res.name;
+      this.merchantName= res.name;
+    },
+    (err) => {
+      console.log(err);
+      // this.services.logout();
+    });
   }
 
+  async confirm():Promise<void>{
+    this.services.topupAtMerchant({
+      amount: this.amount,
+      memberId: this.memberId
+    }).toPromise()
+  }
 }
