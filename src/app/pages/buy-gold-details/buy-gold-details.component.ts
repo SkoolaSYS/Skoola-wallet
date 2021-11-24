59298674444aff3c2c6724fb55105133429d69e2
shakinahsamit <shakinah@komeps.com>
2021-11-24 15:26:05 +0800

    Update interfaces for Sell Gold and Buy Gold

--- src/app/pages/buy-gold-details/buy-gold-details.component.ts
+++ src/app/pages/buy-gold-details/buy-gold-details.component.ts
@@ -32,6 +32,7 @@ senderImg: string = "";
     }
   }
   async otpSubmit(otp: string){
+    // console.log("click icon correct");
       await this.services.buyGoldDetails({
         goldAmount: this.services.buyGold.goldAmount,
         goldPrice: this.services.buyGold.goldPrice,
